93c9cee1abf78f90b00fdd45994e0e5dad6217d1
NeelAvaiya <neelavaiya201@gmail.com>
2026-05-23 15:33:14 +0530

    add signup route

---
+++ apps/web/app/sign-up/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { SignupForm } from "~/components/signup-form";
+
+function SignUpPage() {
+    return (
+        <div>
+            <SignupForm/>
+        </div>
+    );
+}
+
+export default SignUpPage;
---
+++ apps/web/hooks/api/auth/index.ts
@@ -0,0 +1,6 @@
+import { trpc } from "~/trpc/client"
+
+export const useSignup = () => {
+  const { mutateAsync: createUserWithEmailAndPasswordAsync, mutate: createUserWithEmailAndPassword , error, failureCount, isError, isIdle, isSuccess, status } = trpc.auth.createUserWithEmailAndPassword.useMutation()
+  return { createUserWithEmailAndPasswordAsync, createUserWithEmailAndPassword, error, failureCount, isError, isIdle, isSuccess, status }
+}
